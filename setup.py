225dbe7c28c5a207492c1d75a9a9ba5630007eb2
Joongi Kim <joongi@lablup.com>
2015-06-30 15:25:40 +0900

    Fix setup.py version check for protobuf.
     * Alpha/beta/pre versions are "lower" than plain numbers: e.g., 3.0.0a3 < 3.0,
       so we have to add "a3" manually.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     namespace_packages=['sorna'],
 
-    install_requires=['pyzmq', 'aiozmq', 'namedlist', 'protobuf>=3.0'],
+    install_requires=['pyzmq', 'aiozmq', 'namedlist', 'protobuf>=3.0.0a3'],
     extras_require={
         'dev': [],
         'test': [],
