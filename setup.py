a167bc5075219cd1b2ddc7f026566f37f1968f64
Joongi Kim <joongi@lablup.com>
2016-10-06 15:02:18 +0900

    refs lablup/sorna#1: Add license and classifier info.

--- setup.py
+++ setup.py
@@ -12,13 +12,26 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='0.5.0',
     description='Sorna Manager',
     long_description='',
     url='https://github.com/lablup/sorna-manager',
     author='Lablup Inc.',
     author_email='joongi@lablup.com',
-    license='Private',
+    license='LGPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+    ],
 
     packages=['sorna.manager', 'sorna.monitor'],
     namespace_packages=['sorna'],
