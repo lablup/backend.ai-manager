e2b9fd79ab2c379e8a449a55b89aad2d6be8af98
Joongi Kim <joongi@lablup.com>
2016-12-19 16:37:02 +0900

    refs #21: Enforce Python 3.6 in setup.py as well.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Environment :: No Input/Output (Daemon)',
@@ -44,6 +44,7 @@ setup(
     packages=['sorna.manager', 'sorna.gateway', 'sorna.monitor'],
     namespace_packages=['sorna'],
 
+    python_requires='>=3.6',
     install_requires=[
         'ConfigArgParse',
         'coloredlogs',
