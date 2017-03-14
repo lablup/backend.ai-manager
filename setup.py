31cb203e8424891a6338014f620e224ed3ae811f
Joongi Kim <joongi@lablup.com>
2017-03-14 18:58:28 +0900

    Fix sorna-common version requirement in setup.py as well.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'python-dateutil>=2.5',
         'simplejson',
         'uvloop>=0.7',
-        'sorna-common>=0.8,<0.9'],
+        'sorna-common>=0.9,<1.0'],
     extras_require={
         'dev': [],
         'test': ['pytest', 'pytest-mock'],
