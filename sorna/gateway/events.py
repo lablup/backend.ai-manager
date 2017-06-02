9706f6c4d3927bf7972f892823d10997118eaa14
Joongi Kim <joongi@lablup.com>
2017-06-02 16:59:13 +0900

    refs #33: Rewrite to use Subscriber from sorna-common
    Still, dispatching mechanims is hand-brewed one...

--- sorna/gateway/events.py
+++ sorna/gateway/events.py
@@ -1,81 +1,52 @@
 import asyncio
-from collections import defaultdict
-import functools
 import logging
+from collections import defaultdict
 
-import aioredis
-import aiozmq, aiozmq.rpc
+import umsgpack
 
-from . import defs
+from ..msgbus import ExchangeTypes, Subscriber
 
 log = logging.getLogger('sorna.gateway.events')
 
 
-class EventServer(aiozmq.rpc.AttrHandler):
+class AgentEventSubscriber(Subscriber):
+    exchange_name = 'agent-events'
+    exchange_type = ExchangeTypes.DIRECT
+    queue_name = 'events'
 
-    def __init__(self, app, loop=None):
-        self.app = app
-        self.loop = loop if loop else asyncio.get_event_loop()
+    def __init__(self, *args, app=None, **kwargs):
+        kwargs['decoder'] = umsgpack.unpackb
+        super().__init__(*args, **kwargs)
         self.handlers = defaultdict(list)
+        self.app = app
 
-    def add_handler(self, event_name, callback):
-        assert callable(callback)
-        self.handlers[event_name].append(callback)
+    async def init(self, *args, **kwargs):
+        await super().init(*args, **kwargs)
+        await self.subscribe(self._dispatch)
 
-    def local_dispatch(self, event_name, *args, **kwargs):
-        log.debug(f"DISPATCH({event_name}, {str(args[0]) if args else ''})")
-        for handler in self.handlers[event_name]:
+    async def _dispatch(self, body, envelope, props):
+        loop = asyncio.get_event_loop()
+        for handler in self.handlers[body['type']]:
             if asyncio.iscoroutine(handler) or asyncio.iscoroutinefunction(handler):
-                asyncio.ensure_future(handler(self.app, *args, **kwargs))
+                loop.create_task(handler(self.app, body))
             else:
-                cb = functools.partial(handler, self.app, *args, **kwargs)
-                self.loop.call_soon(cb)
+                loop.call_soon(handler(self.app, body))
 
-    @aiozmq.rpc.method
-    def dispatch(self, event_name, *args, **kwargs):
-        self.local_dispatch(event_name, *args, **kwargs)
+    def local_dispatch(self, *args):
+        raise NotImplementedError
 
-
-async def monitor_redis_events(app):
-    redis_sub = await aioredis.create_redis(app.config.redis_addr.as_sockaddr(), encoding='utf8')
-    # Enable "expired" event notification
-    # See more details at: http://redis.io/topics/notifications
-    await redis_sub.config_set('notify-keyspace-events', 'Ex')
-    chprefix = f'__keyevent@{defs.SORNA_INSTANCE_DB}__*'
-    channels = await redis_sub.psubscribe(chprefix)
-    log.debug('monitor_redis_events: subscribed notifications.')
-    try:
-        while True:
-            msg = await channels[0].get(encoding='utf8')
-            if msg is None:
-                break
-            evname = msg[0].decode('ascii').split(':')[1]
-            evkey  = msg[1]
-            if evname == 'expired' and evkey.startswith('shadow:'):
-                inst_id = evkey.split(':', 1)[1]
-                app['event_server'].local_dispatch('instance_terminated', inst_id, 'agent-lost')
-    except asyncio.CancelledError:
-        pass
-    finally:
-        try:
-            await redis_sub.unsubscribe(chprefix)
-        except aioredis.errors.ConnectionClosedError:
-            pass
-        redis_sub.close()
-        await redis_sub.wait_closed()
+    def add_handler(self, key, cb):
+        self.handlers[key].append(cb)
 
 
 async def init(app):
-    app['event_server'] = EventServer(app)
-    app['event_sock'] = await aiozmq.rpc.serve_rpc(
-        app['event_server'],
-        bind=f'tcp://*:{app.config.events_port}')
-    app['event_redis_monitor_task'] = asyncio.ensure_future(monitor_redis_events(app))
+    evs = AgentEventSubscriber(app.config.mq_addr, app=app)
+    await evs.init(
+        user=app.config.mq_user,
+        passwd=app.config.mq_password,
+        vhost=app.config.namespace)
+    app['event_subscriber'] = evs
 
 
 async def shutdown(app):
-    app['event_redis_monitor_task'].cancel()
-    await app['event_redis_monitor_task']
-    await asyncio.sleep(0.01)
-    app['event_sock'].close()
-    await app['event_sock'].wait_closed()
+    await app['event_subscriber'].close()
