cc41153269438592933615b8b79761e37a0ed374
Joongi Kim <joongi@lablup.com>
2017-10-09 03:10:01 +0900

    refs #29: Let it store the configured Redis addr in etcd

--- sorna/gateway/etcd.py
+++ sorna/gateway/etcd.py
@@ -13,16 +13,17 @@ class ConfigServer:
 
     async def register_myself(self):
         instance_id = await get_instance_id()
-        await self.etcd.put(f'nodes/manager', instance_id)
+        await self.etcd.put('nodes/manager', instance_id)
 
     async def deregister_myself(self):
-        await self.etcd.delete_prefix(f'nodes/manager')
+        await self.etcd.delete_prefix('nodes/manager')
 
 
 async def init(app):
     etcd = AsyncEtcd(app.config.etcd_addr, app.config.namespace)
     app['config_server'] = ConfigServer(etcd)
     await app['config_server'].register_myself()
+    await etcd.put('nodes/redis', app.config.redis_addr)
 
 
 async def shutdown(app):
