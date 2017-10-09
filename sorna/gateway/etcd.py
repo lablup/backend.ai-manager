a1cef13463083d487eb812bac21fc64d37b5c947
Joongi Kim <me@daybreaker.info>
2017-10-10 01:02:16 +0900

    events: Store event_addr in etcd as well

--- sorna/gateway/etcd.py
+++ sorna/gateway/etcd.py
@@ -1,7 +1,7 @@
 import logging
 
 from sorna.common.etcd import AsyncEtcd
-from sorna.common.identity import get_instance_id
+from sorna.common.identity import get_instance_id, get_instance_ip
 
 log = logging.getLogger('sorna.gateway.etcd')
 
@@ -11,19 +11,27 @@ class ConfigServer:
     def __init__(self, etcd):
         self.etcd = etcd
 
-    async def register_myself(self):
-        instance_id = await get_instance_id()
+    async def register_myself(self, instance_id):
         await self.etcd.put('nodes/manager', instance_id)
 
     async def deregister_myself(self):
         await self.etcd.delete_prefix('nodes/manager')
 
+    async def update_kernel_images(self):
+        pass
+        # TODO: a method to scan docker hub and update kernel image versions
+        # TODO: a cli command to execute the above method
+
 
 async def init(app):
     etcd = AsyncEtcd(app.config.etcd_addr, app.config.namespace)
+    instance_id = await get_instance_id()
+    instance_ip = await get_instance_ip()
     app['config_server'] = ConfigServer(etcd)
-    await app['config_server'].register_myself()
+    await app['config_server'].register_myself(instance_id)
     await etcd.put('nodes/redis', app.config.redis_addr)
+    event_addr = f'{instance_ip}:{app.config.events_port}'
+    await etcd.put('nodes/manager/event_addr', event_addr)
 
 
 async def shutdown(app):
