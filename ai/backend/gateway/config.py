e6cd4465bc31f46748071eff02118de1b57b094c
Joongi Kim <joongi@lablup.com>
2017-10-16 02:05:14 +0900

    deploy: Use the normal defaults
     * Now Lablup developers should use custom env-var configs
       for coexisting with other internal projects

--- ai/backend/gateway/config.py
+++ ai/backend/gateway/config.py
@@ -12,11 +12,11 @@ def load_config(argv=None, extra_args_func=None):
                help='The TCP port number where the agent instances are listening on.'
                     ' (default: 6001)')
     parser.add('--redis-addr', env_var='BACKEND_REDIS_ADDR', type=host_port_pair,
-               default=HostPortPair(ip_address('127.0.0.1'), 6389),
+               default=HostPortPair(ip_address('127.0.0.1'), 6379),
                help='The hostname-port pair of a redis server. '
                     '(default: localhost:6379)')
     parser.add('--db-addr', env_var='BACKEND_DB_ADDR', type=host_port_pair,
-               default=HostPortPair(ip_address('127.0.0.1'), 5442),
+               default=HostPortPair(ip_address('127.0.0.1'), 5432),
                help='The hostname-port pair of a database server. '
                     '(default: localhost:5432)')
     parser.add('--db-name', env_var='BACKEND_DB_NAME',
