83deef6634104ddea3925ed8e600ffc973fef577
Joongi Kim <joongi@lablup.com>
2018-02-21 02:10:11 +0900

    Remove unnecessary noises in fixture data

--- ai/backend/manager/models/fixtures.py
+++ ai/backend/manager/models/fixtures.py
@@ -6,6 +6,7 @@ example_keypair = [
         'keypairs',
         [
             {
+                # Admin user example key
                 'user_id': 2,  # 1: anonymouse user, 2: default super user
                 'access_key': 'AKIAIOSFODNN7EXAMPLE',
                 'secret_key': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
@@ -16,13 +17,9 @@ example_keypair = [
                 'rate_limit': 1000,
                 'num_queries': 0,
                 'is_admin': True,
-                # Sample free tier: 500 launches per day x 30 days per month
-                # 'remaining_cpu': 180000 * 500 * 30,   # msec (180 sec per launch)
-                # 'remaining_mem': 1048576 * 500 * 30,  # KBytes (1GB per launch)
-                # 'remaining_io': 102400 * 500 * 30,    # KBytes (100MB per launch)
-                # 'remaining_net': 102400 * 500 * 30,   # KBytes (100MB per launch)
             },
             {
+                # Non-admin user example key
                 'user_id': 3,
                 'access_key': 'AKIANOTADMIN7EXAMPLE',
                 'secret_key': 'C8qnIo29EZvXkPK_MXcuAakYTy4NYrxwmCEyNPlf',
@@ -33,11 +30,6 @@ example_keypair = [
                 'rate_limit': 1000,
                 'num_queries': 0,
                 'is_admin': False,
-                # Sample free tier: 500 launches per day x 30 days per month
-                # 'remaining_cpu': 180000 * 500 * 30,   # msec (180 sec per launch)
-                # 'remaining_mem': 1048576 * 500 * 30,  # KBytes (1GB per launch)
-                # 'remaining_io': 102400 * 500 * 30,    # KBytes (100MB per launch)
-                # 'remaining_net': 102400 * 500 * 30,   # KBytes (100MB per launch)
             },
         ]
     ),
