faceb8716b8f2dadbfe59c20a743f82e093a6bc7
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-30 12:53:47 +0900

    Add API for fetching manager status (#106)

--- src/ai/backend/gateway/manager.py
+++ src/ai/backend/gateway/manager.py
@@ -2,12 +2,14 @@ import asyncio
 import enum
 import functools
 import logging
+import sqlalchemy as sa
 
 from aiohttp import web
 
 from ai.backend.common.logging import BraceStyleAdapter
 
 from .exceptions import InvalidAPIParameters, ServerFrozen
+from ..manager.models import kernels, KernelStatus
 
 
 log = BraceStyleAdapter(logging.getLogger('ai.backend.gateway.manager'))
@@ -64,6 +66,26 @@ async def detect_status_update(app):
                   app['pidx'], updated_status)
 
 
+async def fetch_manager_status(request):
+    try:
+        status = await request.app['config_server'].get_manager_status()
+
+        async with request.app['dbpool'].acquire() as conn, conn.begin():
+            query = (sa.select([sa.func.count(kernels.c.id)])
+                       .select_from(kernels)
+                       .where((kernels.c.role == 'master') &
+                              (kernels.c.status != KernelStatus.TERMINATED)))
+            active_sessions_num = await conn.scalar(query)
+
+            return web.json_response({
+                'status': status.value,
+                'active_sessions': active_sessions_num,
+            })
+    except:
+        log.exception('GET_MANAGER_STATUS: exception')
+        raise
+
+
 async def update_manager_status(request):
     params = await request.json()
     try:
@@ -94,7 +116,7 @@ async def shutdown(app):
 def create_app():
     app = web.Application()
     app['api_versions'] = (2, 3)
-    app.router.add_route('GET', r'/status', get_manager_status)
+    app.router.add_route('GET', r'/status', fetch_manager_status)
     app.router.add_route('PUT', r'/status', update_manager_status)
     app.on_startup.append(init)
     app.on_shutdown.append(shutdown)
