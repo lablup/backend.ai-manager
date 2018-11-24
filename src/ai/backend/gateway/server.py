9d4ca3331eafbc66ecc7309ce3619bc5c7c6d3cb
Joongi Kim <joongi@lablup.com>
2018-11-24 23:18:56 +0900

    Update the latest API version and fix Server response header

--- src/ai/backend/gateway/server.py
+++ src/ai/backend/gateway/server.py
@@ -55,7 +55,7 @@ VALID_VERSIONS = frozenset([
     'v3.20170615',
     'v4.20181215',
 ])
-LATEST_API_VERSION = 'v3.20170615'
+LATEST_API_VERSION = 'v4.20181215'
 
 log = BraceStyleAdapter(logging.getLogger('ai.backend.gateway.server'))
 
@@ -155,7 +155,6 @@ async def legacy_auth_test_redirect(request):
 async def gw_init(app):
     # should be done in create_app() in other modules.
     app.router.add_route('GET', r'', hello)
-    app.on_response_prepare.append(on_prepare)
 
     # legacy redirects
     app.router.add_route('GET', r'/v{version:\d+}/authorize',
@@ -290,6 +289,7 @@ async def server_main(loop, pidx, _args):
             allow_credentials=False,
             expose_headers="*", allow_headers="*"),
     }
+    app.on_response_prepare.append(on_prepare)
     cors = aiohttp_cors.setup(app, defaults=cors_options)
     app['config'] = _args[0]
     app['sslctx'] = None
@@ -324,6 +324,7 @@ async def server_main(loop, pidx, _args):
     def init_subapp(create_subapp):
         subapp, global_middlewares = create_subapp()
         assert isinstance(subapp, web.Application)
+        subapp.on_response_prepare.append(on_prepare)
         # Allow subapp's access to the root app properties.
         # These are the public APIs exposed to extensions as well.
         subcors = aiohttp_cors.setup(subapp)
