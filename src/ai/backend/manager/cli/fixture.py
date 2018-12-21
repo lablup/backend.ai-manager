8b1bb37ab0e58baeed37d92fb9f17a3c5e3a44e0
Joongi Kim <joongi@lablup.com>
2018-12-21 17:33:17 +0900

    Change fixtures to use static JSON files

--- src/ai/backend/manager/cli/fixture.py
+++ src/ai/backend/manager/cli/fixture.py
@@ -1,4 +1,6 @@
 import logging
+import json
+from pathlib import Path
 
 import sqlalchemy as sa
 
@@ -6,7 +8,6 @@ from ai.backend.common.logging import BraceStyleAdapter
 
 from . import register_command
 from .. import models
-from ..models import fixtures
 
 log = BraceStyleAdapter(logging.getLogger(__name__))
 
@@ -20,9 +21,9 @@ def fixture(args):
 @fixture.register_command
 def populate(args):
     '''Populate fixtures.'''
-    log.info("populating fixture '{0}'", args.fixture_name)
+    log.info("populating fixture '{0}'", args.fixture_path)
     try:
-        fixture = getattr(fixtures, args.fixture_name)
+        fixture = json.loads(args.fixture_path.read_text(encoding='utf8'))
     except AttributeError:
         log.error('No such fixture.')
         return
@@ -30,19 +31,17 @@ def populate(args):
     engine = sa.create_engine(f"postgres://{args.db_user}:{args.db_password}"
                               f"@{args.db_addr}/{args.db_name}")
     conn = engine.connect()
-    for rowset in fixture:
-        table = getattr(models, rowset[0])
-        conn.execute(table.insert(), rowset[1])
+    for table_name, rows in fixture.items():
+        table = getattr(models, table_name)
+        conn.execute(table.insert(), rows)
     conn.close()
 
 
-populate.add('fixture_name', type=str, help='The name of fixture.')
+populate.add('fixture_path', type=Path,
+             help='The path to a fixture dataset written as JSON.')
 
 
 @fixture.register_command
 def list(args):
     '''List all available fixtures.'''
-    for fixture_name in fixtures.__all__:
-        f = getattr(fixtures, fixture_name)
-        stat = map(lambda rowset: f"{rowset[0]}:{len(rowset[1])}", f)
-        print(f"{fixture_name} ({', '.join(stat)})")
+    print('WARNING: This command is deprecated.')
