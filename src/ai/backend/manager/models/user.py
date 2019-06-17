eca9af40fbf048392b2df51a0ab97fcbba172f89
Jonghyun Park <adrysn@gmail.com>
2019-06-17 14:35:53 +0900

    Eaiser to understand user list parsing

--- src/ai/backend/manager/models/user.py
+++ src/ai/backend/manager/models/user.py
@@ -115,17 +115,15 @@ class User(graphene.ObjectType):
                 query = query.where(users.c.domain_name == context['user']['domain_name'])
             if is_active is not None:
                 query = query.where(users.c.is_active == is_active)
-            objs = []
-            emails = []
+            objs_per_key = OrderedDict()
             async for row in conn.execute(query):
-                if row.email in emails:
-                    # If same user is already saved, just append group name
-                    idx = emails.index(row.email)
-                    objs[idx].groups.append(UserGroup(id=str(row.id), name=row.name))
+                if row.email in objs_per_key:
+                    # If same user is already saved, just append group information.
+                    objs_per_key[row.email].groups.append(UserGroup(id=row.id, name=row.name))
                     continue
-                emails.append(row.email)
                 o = User.from_row(row)
-                objs.append(o)
+                objs_per_key[row.email] = o
+            objs = list(objs_per_key.values())
         return objs
 
     @staticmethod
