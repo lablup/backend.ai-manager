6c2f3855a09d7683377e156cc84a8af915c5b9f4
Joongi Kim <joongi@lablup.com>
2019-07-25 22:19:47 +0900

    Add common query function

--- src/ai/backend/manager/models/scaling_group.py
+++ src/ai/backend/manager/models/scaling_group.py
@@ -1,5 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql as pgsql
+from typing import Union
+import uuid
 
 from .base import metadata
 
@@ -22,8 +24,8 @@ scaling_groups = sa.Table(
 # each domain, group, and keypair.
 
 
-allowed_scaling_groups_for_domains = sa.Table(
-    'allowed_scaling_groups_for_domains', metadata,
+sgroups_for_domains = sa.Table(
+    'sgroups_for_domains', metadata,
     sa.Column('scaling_group',
               sa.ForeignKey('scaling_groups.name',
                             onupdate='CASCADE',
@@ -38,8 +40,8 @@ allowed_scaling_groups_for_domains = sa.Table(
 )
 
 
-allowed_scaling_groups_for_groups = sa.Table(
-    'allowed_scaling_groups_for_groups', metadata,
+sgroups_for_groups = sa.Table(
+    'sgroups_for_groups', metadata,
     sa.Column('scaling_group',
               sa.ForeignKey('scaling_groups.name',
                             onupdate='CASCADE',
@@ -54,8 +56,8 @@ allowed_scaling_groups_for_groups = sa.Table(
 )
 
 
-allowed_scaling_groups_for_keypairs = sa.Table(
-    'allowed_scaling_groups_for_keypairs', metadata,
+sgroups_for_keypairs = sa.Table(
+    'sgroups_for_keypairs', metadata,
     sa.Column('scaling_group',
               sa.ForeignKey('scaling_groups.name',
                             onupdate='CASCADE',
@@ -68,3 +70,35 @@ allowed_scaling_groups_for_keypairs = sa.Table(
               index=True, nullable=False),
     sa.UniqueConstraint('scaling_group', 'access_key', name='uq_sgroup_akey'),
 )
+
+
+async def query_allowed_sgroups(db_conn: object,
+                                domain: str,
+                                group: Union[uuid.UUID, str],
+                                access_key: str):
+    query = (sa.select([sgroups_for_domains.c.scaling_group])
+               .where(sgroups_for_domains.c.name == domain))
+    result = await db_conn.execute(query)
+    from_domain = {row['scaling_group'] async for row in result}
+
+    if isinstance(group, uuid.UUID):
+        query = (sa.select([sgroups_for_groups.c.scaling_group])
+                   .where(sgroups_for_groups.c.id == group))
+    elif isinstance(group, str):
+        query = (sa.select([sgroups_for_groups.c.scaling_group])
+                   .where(sgroups_for_groups.c.name == group))
+    else:
+        raise ValueError('unexpected type for group', group)
+    result = await db_conn.execute(query)
+    from_group = {row['scaling_group'] async for row in result}
+
+    query = (sa.select([sgroups_for_keypairs.c.scaling_group])
+               .where(sgroups_for_keypairs.c.access_key == access_key))
+    result = await db_conn.execute(query)
+    from_keypair = {row['scaling_group'] async for row in result}
+
+    sgroups = from_domain | from_group | from_keypair
+    query = (sa.select([scaling_groups])
+               .where(scaling_groups.c.name.in_(sgroups)))
+    result = await db_conn.execute(query)
+    return [row async for row in result]
