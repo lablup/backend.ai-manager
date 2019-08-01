6e2f3617c2bc1c987ba96decf941138447c11060
Joongi Kim <joongi@lablup.com>
2019-08-02 00:02:37 +0900

    Fix refresh_session callback not called (patch from 19.03)
    * refs a previous commit 2c46f267c
    
    * functools.partial() against a coroutine function returns a
      non-coroutine function though the returned function may be used
      in the same way.
    
    * To keep the "coroutine function" type, we need to use
      aiotools.apartial().
    
    * Fix the test for call_non_bursty() which has been changed to reject
      coroutine objects

--- tests/gateway/test_utils.py
+++ tests/gateway/test_utils.py
@@ -21,6 +21,7 @@ async def test_call_non_bursty():
     await asyncio.sleep(0.11)
 
     # check run as coroutine
+    execution_count = 0
     with pytest.raises(TypeError):
         await call_non_bursty(key, execute())
 
