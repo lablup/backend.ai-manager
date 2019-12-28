e65a5b26ea1f24fa03f643bd8a758e5daf2f8dc4
Joongi Kim <joongi@lablup.com>
2019-12-28 15:01:11 +0900

    Update tests for API version checks and deprecate v1 to v3

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -13,22 +13,22 @@ async def test_api_method_override(test_client):
         observed_method = request.method
         return web.Response(body=b'test')
 
-    app.router.add_route('POST', r'/v{version:\d+}/test',
+    app.router.add_route('POST', r'/test',
                          method_placeholder('REPORT'))
-    app.router.add_route('REPORT', r'/v{version:\d+}/test',
+    app.router.add_route('REPORT', r'/test',
                          service_handler)
     app.middlewares.append(api_middleware)
     client = await test_client(app)
 
     # native method
-    resp = await client.request('REPORT', '/v2/test')
+    resp = await client.request('REPORT', '/test')
     assert resp.status == 200
     assert (await resp.read()) == b'test'
     assert observed_method == 'REPORT'
 
     # overriden method
     observed_method = None
-    resp = await client.post('/v2/test', headers={
+    resp = await client.post('/test', headers={
         'X-Method-Override': 'REPORT',
     })
     assert resp.status == 200
@@ -37,13 +37,13 @@ async def test_api_method_override(test_client):
 
     # calling placeholder
     observed_method = None
-    resp = await client.post('/v2/test')
+    resp = await client.post('/test')
     assert resp.status == 405
     assert observed_method is None
 
     # calling with non-relevant method
     observed_method = None
-    resp = await client.delete('/v2/test')
+    resp = await client.delete('/test')
     assert resp.status == 405
     assert observed_method is None
 
@@ -62,27 +62,27 @@ async def test_api_method_override_with_different_ops(test_client):
         observed_method = request.method
         return web.Response(body=b'op2')
 
-    app.router.add_route('POST', r'/v{version:\d+}/test', op1_handler)
-    app.router.add_route('REPORT', r'/v{version:\d+}/test', op2_handler)
+    app.router.add_route('POST', r'/test', op1_handler)
+    app.router.add_route('REPORT', r'/test', op2_handler)
     app.middlewares.append(api_middleware)
     client = await test_client(app)
 
     # native method
-    resp = await client.request('POST', '/v2/test')
+    resp = await client.request('POST', '/test')
     assert resp.status == 200
     assert (await resp.read()) == b'op1'
     assert observed_method == 'POST'
 
     # native method
     observed_method = None
-    resp = await client.request('REPORT', '/v2/test')
+    resp = await client.request('REPORT', '/test')
     assert resp.status == 200
     assert (await resp.read()) == b'op2'
     assert observed_method == 'REPORT'
 
     # overriden method
     observed_method = None
-    resp = await client.request('REPORT', '/v2/test', headers={
+    resp = await client.request('REPORT', '/test', headers={
         'X-Method-Override': 'POST',
     })
     assert resp.status == 200
@@ -91,7 +91,7 @@ async def test_api_method_override_with_different_ops(test_client):
 
     # overriden method
     observed_method = None
-    resp = await client.request('POST', '/v2/test', headers={
+    resp = await client.request('POST', '/test', headers={
         'X-Method-Override': 'REPORT',
     })
     assert resp.status == 200
@@ -108,33 +108,27 @@ async def test_api_ver(test_client):
         inner_request = request
         return web.Response(body=b'test')
 
-    app.router.add_post(r'/v{version:\d+}/test', dummy_handler)
+    app.router.add_post(r'/test', dummy_handler)
     app.middlewares.append(api_middleware)
     client = await test_client(app)
 
     # normal call
-    resp = await client.post('/v1/test', headers={
-        'X-BackendAI-Version': 'v1.20160915',
+    resp = await client.post('/test', headers={
+        'X-BackendAI-Version': 'v5.20191215',
     })
     assert resp.status == 200
-    assert inner_request['api_version'][0] == 1
+    assert inner_request['api_version'][0] == 5
 
-    # calling without version prefix
+    # normal call with different version
+    resp = await client.post('/test', headers={
+        'X-BackendAI-Version': 'v4.20190615',
+    })
+    assert resp.status == 200
+    assert inner_request['api_version'][0] == 4
+
+    # calling with invalid/deprecated version
     resp = await client.post('/test', headers={
         'X-BackendAI-Version': 'v2.20170315',
     })
-    assert resp.status == 404
-
-    # normal call with different version
-    resp = await client.post('/v2/test', headers={
-        'X-BackendAI-Version': 'v2.20170315',
-    })
-    assert resp.status == 200
-    assert inner_request['api_version'][0] == 2
-
-    # calling with invalid version
-    resp = await client.post('/v0/test', headers={
-        'X-BackendAI-Version': 'v2.20170315',
-    })
     assert resp.status == 400
-    assert 'Unsupported' in (await resp.text())
+    assert 'Unsupported' in (await resp.json())['msg']
