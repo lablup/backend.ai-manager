635f5ba732e64bd0bbb34c11f4e654c70919afb9
Kyujin Cho <kyujin.cho@lablup.com>
2020-01-17 18:08:36 +0900

    Pass dotfile to agent when creating kernel

--- src/ai/backend/manager/registry.py
+++ src/ai/backend/manager/registry.py
@@ -590,14 +590,16 @@ class AgentRegistry:
 
         # Create kernel object in PENDING state.
         async with self.dbpool.acquire() as conn, conn.begin():
-            # Feed SSH keypair if exists.
-            query = (sa.select([keypairs.c.ssh_public_key, keypairs.c.ssh_private_key])
+            # Feed SSH keypair and dotfiles if exists.
+            query = (sa.select([keypairs.c.ssh_public_key, keypairs.c.ssh_private_key, keypairs.c.dotfiles])
                        .select_from(keypairs)
                        .where(keypairs.c.access_key == access_key))
             result = await conn.execute(query)
             row  = await result.fetchone()
+            dotfiles = msgpack.unpackb(row['dotfiles'])
+            internal_data = {} if internal_data is None else internal_data
+            internal_data.update({'dotfiles': dotfiles})
             if row['ssh_public_key'] and row['ssh_private_key']:
-                internal_data = {} if internal_data is None else internal_data
                 internal_data['ssh_keypair'] = {
                     'public_key': row['ssh_public_key'],
                     'private_key': row['ssh_private_key'],
