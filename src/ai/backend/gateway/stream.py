01474f733c39fd519ca295ea52caa5f3a47012cd
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-01-29 14:42:07 +0900

    Made aliase of image-defined ports

--- src/ai/backend/gateway/stream.py
+++ src/ai/backend/gateway/stream.py
@@ -358,6 +358,9 @@ async def stream_proxy(request: web.Request, params: Mapping[str, Any]) -> web.S
                 port = sport['host_ports'][0]
             dest = (kernel_host, port)
             break
+        elif service in map(str, sport.get('host_ports', [])):
+            port = int(service)
+            break
     else:
         raise AppNotFound(f'{session_name}:{service}')
 
