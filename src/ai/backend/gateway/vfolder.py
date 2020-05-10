b00cf3c9028be30fc0b26ef3558ad47ebbfb4603
Jonghyun Park <adrysn@gmail.com>
2020-05-10 22:10:23 +0900

    refactor: use async iterator for directory walking and reading chunks

--- src/ai/backend/gateway/vfolder.py
+++ src/ai/backend/gateway/vfolder.py
@@ -6,6 +6,7 @@ import os
 from pathlib import Path
 import shutil
 import stat
+import time
 from typing import (
     Any, Awaitable, Callable,
     Dict, Mapping, MutableMapping,
@@ -911,12 +912,48 @@ async def delete_files(request: web.Request, params: Any, row: VFolderRow) -> we
     return web.json_response(resp, status=200)
 
 
-async def download_directory_as_archive(request, file_path, zip_filename=None, relative_to=None):
+async def download_directory_as_archive(request: web.Request,
+                                        file_path: Path,
+                                        zip_filename: str=None) -> web.Response:
+    """Serve a directory as a zip archive on the fly."""
+    def _iter2aiter(iter):
+        """Iterable to async iterable"""
+        def _consume(loop, iter, q):
+            for item in iter:
+                while True:
+                    if not q.full():
+                        q.put(item)
+                        break
+                    time.sleep(0.01)
+            while True:
+                if not q.full():
+                    q.put(eof_sentinel)
+                    break
+                time.sleep(0.01)
+
+        async def _aiter():
+            loop = current_loop()
+            q = janus.Queue(maxsize=DEFAULT_INFLIGHT_CHUNKS)
+            try:
+                fut = loop.run_in_executor(None, lambda: _consume(loop, iter, q.sync_q))
+                while True:
+                    item = await q.async_q.get()
+                    if item is eof_sentinel:
+                        break
+                    yield item
+                    q.async_q.task_done()
+                await fut
+            finally:
+                q.close()
+                await q.wait_closed()
+
+        return _aiter()
+
     if zip_filename is None:
         zip_filename = file_path.name + '.zip'
     # zf = zipstream.ZipFile(compression=zipstream.ZIP_DEFLATED)
     zf = zipstream.ZipFile()
-    for root, dirs, files in os.walk(file_path):
+    async for root, dirs, files in _iter2aiter(os.walk(file_path)):
         for file in files:
             zf.write(Path(root) / file, Path(root).relative_to(file_path) / file)
         if len(dirs) == 0 and len(files) == 0:
@@ -927,12 +964,12 @@ async def download_directory_as_archive(request, file_path, zip_filename=None, r
         hdrs.CONTENT_TYPE: 'application/zip',
         hdrs.CONTENT_DISPOSITION: " ".join([
             "attachment;"
-            f"filename=\"{ascii_filename}\";",
-            f"filename*=UTF-8''{encoded_filename}",
+            f"filename=\"{ascii_filename}\";",       # RFC-2616 sec2.2
+            f"filename*=UTF-8''{encoded_filename}",  # RFC-5987
         ])
     })
     await response.prepare(request)
-    for chunk in zf:
+    async for chunk in _iter2aiter(zf):
         await response.write(chunk)
     return response
 
@@ -995,7 +1032,7 @@ async def download_single(request: web.Request, params: Any, row: VFolderRow) ->
     except (ValueError, FileNotFoundError):
         raise GenericNotFound('The file is not found.')
     if not file_path.is_file():
-        raise InvalidAPIParameters('The file is not a regular file.')
+        return await download_directory_as_archive(request, file_path)
     if request.method == 'HEAD':
         return web.Response(status=200, headers={
             hdrs.ACCEPT_RANGES: 'bytes',
@@ -1075,7 +1112,6 @@ async def download_with_token(request) -> web.StreamResponse:
     except (ValueError, FileNotFoundError):
         raise InvalidAPIParameters('The file is not found.')
     if not file_path.is_file():
-        raise InvalidAPIParameters('The file is not a regular file.')
         return await download_directory_as_archive(request, file_path)
     if request.method == 'HEAD':
         return web.Response(status=200, headers={
