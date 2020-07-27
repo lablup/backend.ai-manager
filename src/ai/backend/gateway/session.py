1918bb8acf5f70b69e355607f926e77b3b4ebff0
Joongi Kim <joongi@lablup.com>
2020-07-27 13:33:32 +0900

    fix: Return the session UUID after creating sessions (#303)
    * This is the intention to APIv5 revision, while I mistakenly fixed
      the "reuse" case only with incomplete implementation.
    * Include the overlapping session ID in "session already exists" errors

--- src/ai/backend/gateway/session.py
+++ src/ai/backend/gateway/session.py
@@ -244,27 +244,31 @@ async def _create(request: web.Request, params: Any, dbpool) -> web.Response:
     except AliasResolutionFailed:
         raise ImageNotFound
 
-    # Check existing (owner_access_key, session) kernel instance
+    # Check existing (owner_access_key, session_name) instance
     try:
         # NOTE: We can reuse the session IDs of TERMINATED sessions only.
         # NOTE: Reusing a session in the PENDING status returns an empty value in service_ports.
         kern = await registry.get_session(params['session_name'], owner_access_key)
         running_image_ref = ImageRef(kern['image'], [kern['registry']])
         if running_image_ref != requested_image_ref:
-            raise SessionAlreadyExists
-        create = False
-    except SessionNotFound:
-        create = True
-    if not create:
+            # The image must be same if get_or_create() called multiple times
+            # against an existing (non-terminated) session
+            raise SessionAlreadyExists(extra_data={'existingSessionId': str(kern['id'])})
         if not params['reuse']:
-            raise SessionAlreadyExists
+            # Respond as error since the client did not request to reuse,
+            # but provide the overlapping session ID for later use.
+            raise SessionAlreadyExists(extra_data={'existingSessionId': str(kern['id'])})
+        # Respond as success with the reused session's information.
         return web.json_response({
-            'sessionId': None,   # TODO: will be implemented in multi-container session
+            'sessionId': str(kern['id']),
             'sessionName': str(kern['sess_id']),
             'status': kern['status'].name,
             'service_ports': kern['service_ports'],
             'created': False,
         }, status=200)
+    except SessionNotFound:
+        # It's time to create a new session.
+        pass
 
     if params['session_type'] == SessionTypes.BATCH and not params['startup_command']:
         raise InvalidAPIParameters('Batch sessions must have a non-empty startup command.')
@@ -402,7 +406,8 @@ async def _create(request: web.Request, params: Any, dbpool) -> web.Response:
             session_tag=params['tag'],
             starts_at=starts_at,
         ))
-        resp['sessionId'] = str(params['session_name'])  # legacy naming
+        resp['sessionId'] = str(kernel_id)  # changed since API v5
+        resp['sessionName'] = str(params['session_name'])
         resp['status'] = 'PENDING'
         resp['servicePorts'] = []
         resp['created'] = True
