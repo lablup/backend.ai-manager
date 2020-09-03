cd5696b39b430723d6d88fe96a3e7c1da6c7ed74
Sergey Leksikov <lexikovs@gmail.com>
2020-09-03 10:23:38 +0900

    Fixed all issues

--- scripts/migrate-vfolder-structure.py
+++ scripts/migrate-vfolder-structure.py
@@ -8,33 +8,26 @@ def main():
     print(input_path, output_path)
     subfolders = [x for x in input_path.iterdir() if x.is_dir()]
 
-    print("Subfolders ", subfolders)
+    print("Number of folders ", len(subfolders))
+    try:
 
-    for folder in subfolders:
-        folder = str(folder)
-        folder = folder.split("/")[-1]
-        header_dir = Path(folder[0:2])
-        second_lvl_dir = Path(folder[2:4])
-        rest_dir = Path(folder[4:])
+        for folder in subfolders:
+            folder = str(folder)
+            folder = folder.split("/")[-1]
+            header_dir = Path(folder[0:2])
+            second_lvl_dir = Path(folder[2:4])
+            rest_dir = Path(folder[4:])
 
-        root = Path(input_path / Path(folder))
-        list_of_files = [x for x in root.iterdir() if x.is_file()]
-        print("list of files ", list_of_files)
-
-        try:
-            if not Path.is_dir(output_path / header_dir):
-                Path.mkdir(output_path / header_dir)
-            if not Path.is_dir(output_path / header_dir / second_lvl_dir):
-                Path.mkdir(output_path / header_dir / second_lvl_dir)
-            if not Path.is_dir(output_path / header_dir / second_lvl_dir / rest_dir):
-                Path.mkdir(output_path / header_dir / second_lvl_dir / rest_dir)
+            Path(output_path / header_dir).mkdir(parents=True, exist_ok=True)
+            Path(output_path / header_dir / second_lvl_dir).mkdir(parents=True, exist_ok=True)
+            Path(output_path / header_dir / second_lvl_dir / rest_dir).mkdir(parents=True, exist_ok=True)
 
             Path.rename(input_path / folder, output_path / header_dir / second_lvl_dir / rest_dir)
 
-        except OSError:
-            print("Creation of the directories failed")
-        finally:
-            print("Successfully created the directories")
+    except OSError:
+        print("Creation of the directories failed")
+    finally:
+        print("Successfully created the directories")
 
 
 if __name__ == '__main__':
