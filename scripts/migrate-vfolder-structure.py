ec5a3891640fc555c4245aaadd29b012b9b7340b
Sergey Leksikov <lexikovs@gmail.com>
2020-09-03 14:03:09 +0900

    Update scripts/migrate-vfolder-structure.py
    Co-authored-by: Joongi Kim <joongi@lablup.com>

--- scripts/migrate-vfolder-structure.py
+++ scripts/migrate-vfolder-structure.py
@@ -14,13 +14,11 @@ def main():
         for folder in subfolders:
             folder = str(folder)
             folder = folder.split("/")[-1]
-            header_dir = Path(folder[0:2])
-            second_lvl_dir = Path(folder[2:4])
-            rest_dir = Path(folder[4:])
+            header_dir = folder[0:2]
+            second_lvl_dir = folder[2:4]
+            rest_dir = folder[4:]
 
-            Path(output_path / header_dir).mkdir(parents=True, exist_ok=True)
-            Path(output_path / header_dir / second_lvl_dir).mkdir(parents=True, exist_ok=True)
-            Path(output_path / header_dir / second_lvl_dir / rest_dir).mkdir(parents=True, exist_ok=True)
+            (output_path / header_dir / second_lvl_dir / rest_dir).mkdir(parents=True, exist_ok=True)
 
             Path.rename(input_path / folder, output_path / header_dir / second_lvl_dir / rest_dir)
 
