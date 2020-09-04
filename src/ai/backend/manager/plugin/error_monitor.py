lablup/backend.ai-manager

commit ed130d884e908dc8894617fb0f053cdec6e4b9eb

Fix error monitor plugin inheritance (#321)

src/ai/backend/manager/plugin/error_monitor.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import traceback
-from typing import Any, Optional, Mapping
+from typing import Any, Mapping
 
 from aiohttp import web
 
@@ -9,14 +9,14 @@ from ai.backend.common.logging import BraceStyleAdapter
 from ai.backend.common.types import (
     AgentId
 )
-from ai.backend.common.plugin import AbstractPlugin
+from ai.backend.common.plugin.monitor import AbstractErrorReporterPlugin
 from ..models import error_logs, LogSeverity
 from .exceptions import PluginError
 
-log = BraceStyleAdapter(logging.getLogger('ai.backend.gateway.server'))
+log = BraceStyleAdapter(logging.getLogger(__name__))
 
 
-class ErrorMonitor(AbstractPlugin):
+class ErrorMonitor(AbstractErrorReporterPlugin):
     async def init(self, context: Any = None) -> None:
         if context is None or 'app' not in context:
             raise PluginError('App was not passed in to ErrorMonitor plugin')
@@ -30,16 +30,26 @@ class ErrorMonitor(AbstractPlugin):
     async def update_plugin_config(self, plugin_config: Mapping[str, Any]) -> None:
         pass
 
-    async def capture_exception(self, exc: Optional[BaseException] = None, user: Any = None,
-                                context_env: Any = None,
-                                severity: Optional[LogSeverity] = None):
-        if exc:
-            tb = exc.__traceback__
+    async def capture_message(self, message: str) -> None:
+        pass
+
+    async def capture_exception(self, exc_instance: Exception = None,
+                                context: Mapping[str, Any] = None) -> None:
+        if exc_instance:
+            tb = exc_instance.__traceback__
         else:
             _, exc, tb = sys.exc_info()
         exc_type: Any = type(exc)
-        if severity is None:
+
+        if context is None or 'severity' not in context:
             severity = LogSeverity.ERROR
+        else:
+            severity = context['severity']
+        if context is None or 'user' not in context:
+            user = None
+        else:
+            user = context['user']
+
         async with self.dbpool.acquire() as conn, conn.begin():
             query = error_logs.insert().values({
                 'severity': severity,
@@ -47,7 +57,7 @@ class ErrorMonitor(AbstractPlugin):
                 'user': user,
                 'message': ''.join(traceback.format_exception_only(exc_type, exc)).strip(),
                 'context_lang': 'python',
-                'context_env': context_env,
+                'context_env': context,
                 'traceback': ''.join(traceback.format_tb(tb)).strip()
             })
             await conn.execute(query)
@@ -57,7 +67,7 @@ class ErrorMonitor(AbstractPlugin):
                                  message: str,
                                  traceback: str = None,
                                  user: Any = None,
-                                 context_env: Any = None,
+                                 context_env: Mapping[str, Any] = None,
                                  severity: LogSeverity = None) -> None:
         if severity is None:
             severity = LogSeverity.ERROR

src/ai/backend/gateway/server.py
@@ -420,7 +420,7 @@ def handle_loop_error(
             exc_info = (type(exception), exception, exception.__traceback__)
             log.error('Error inside event loop: {0}', msg, exc_info=exc_info)
             if 'error_monitor' in root_app:
-                loop.create_task(root_app['error_monitor'].capture_exception(exception))
+                loop.create_task(root_app['error_monitor'].capture_exception(exc_instance=exception))
 
 
 def _init_subapp(pkg_name: str,

src/ai/backend/gateway/session.py
@@ -462,7 +462,7 @@ async def _create(request: web.Request, params: Any, dbpool) -> web.Response:
     except UnknownImageReference:
         raise InvalidAPIParameters(f"Unknown image reference: {params['image']}")
     except Exception:
-        await request.app['error_monitor'].capture_exception(user=owner_uuid)
+        await request.app['error_monitor'].capture_exception(context={'user': owner_uuid})
         log.exception('GET_OR_CREATE: unexpected error!')
         raise InternalServerError
     finally:
@@ -969,7 +969,7 @@ async def restart(request: web.Request) -> web.Response:
         log.exception('RESTART: exception')
         raise
     except:
-        await request.app['error_monitor'].capture_exception(user=request['user']['uuid'])
+        await request.app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
         log.exception('RESTART: unexpected error')
         raise web.HTTPInternalServerError
     return web.Response(status=204)
@@ -1181,7 +1181,7 @@ async def download_files(request: web.Request, params: Any) -> web.Response:
     except (ValueError, FileNotFoundError):
         raise InvalidAPIParameters('The file is not found.')
     except Exception:
-        await request.app['error_monitor'].capture_exception(user=request['user']['uuid'])
+        await request.app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
         log.exception('DOWNLOAD_FILE: unexpected error!')
         raise InternalServerError
 
@@ -1218,7 +1218,7 @@ async def download_single(request: web.Request, params: Any) -> web.Response:
     except (ValueError, FileNotFoundError):
         raise InvalidAPIParameters('The file is not found.')
     except Exception:
-        await request.app['error_monitor'].capture_exception(user=request['user']['uuid'])
+        await request.app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
         log.exception('DOWNLOAD_SINGLE: unexpected error!')
         raise InternalServerError
     return web.Response(body=result, status=200)
@@ -1252,7 +1252,7 @@ async def list_files(request: web.Request) -> web.Response:
         log.exception('LIST_FILES: exception')
         raise
     except Exception:
-        await request.app['error_monitor'].capture_exception(user=request['user']['uuid'])
+        await request.app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
         log.exception('LIST_FILES: unexpected error!')
         raise InternalServerError
     return web.json_response(resp, status=200)

src/ai/backend/gateway/stream.py
@@ -164,7 +164,7 @@ async def stream_pty(defer, request: web.Request) -> web.StreamResponse:
             # Agent or kernel is terminated.
             raise
         except Exception:
-            await app['error_monitor'].capture_exception(user=request['user']['uuid'])
+            await app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
             log.exception('stream_stdin({0}): unexpected error', stream_key)
         finally:
             log.debug('stream_stdin({0}): terminated', stream_key)
@@ -196,7 +196,7 @@ async def stream_pty(defer, request: web.Request) -> web.StreamResponse:
         except asyncio.CancelledError:
             pass
         except:
-            await app['error_monitor'].capture_exception(user=request['user']['uuid'])
+            await app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
             log.exception('stream_stdout({0}): unexpected error', stream_key)
         finally:
             log.debug('stream_stdout({0}): terminated', stream_key)
@@ -208,7 +208,7 @@ async def stream_pty(defer, request: web.Request) -> web.StreamResponse:
         stdout_task = asyncio.create_task(stream_stdout())
         await stream_stdin()
     except Exception:
-        await app['error_monitor'].capture_exception(user=request['user']['uuid'])
+        await app['error_monitor'].capture_exception(context={'user': request['user']['uuid']})
         log.exception('stream_pty({0}): unexpected error', stream_key)
     finally:
         stdout_task.cancel()

commit ae6e7f8ab991170e23c85bf0aad56d63825dbbbf

fix: type error introduced in #321

src/ai/backend/manager/plugin/error_monitor.py
@@ -41,7 +41,14 @@ class ErrorMonitor(AbstractErrorReporterPlugin):
         if exc_instance:
             tb = exc_instance.__traceback__
         else:
-            _, exc_instance, tb = sys.exc_info()
+            _, sys_exc_instance, tb = sys.exc_info()
+            if (
+                isinstance(sys_exc_instance, BaseException)
+                and not isinstance(sys_exc_instance, Exception)
+            ):
+                # bypass BaseException as they are used for controlling the process/coroutine lifecycles
+                # instead of indicating actual errors
+                return
         exc_type: Any = type(exc_instance)
 
         if context is None or 'severity' not in context:
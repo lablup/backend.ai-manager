ae6e7f8ab991170e23c85bf0aad56d63825dbbbf
Joongi Kim <joongi@lablup.com>
2020-09-04 10:54:25 +0900

    fix: type error introduced in #321

--- src/ai/backend/manager/plugin/error_monitor.py
+++ src/ai/backend/manager/plugin/error_monitor.py
@@ -41,7 +41,14 @@ class ErrorMonitor(AbstractErrorReporterPlugin):
         if exc_instance:
             tb = exc_instance.__traceback__
         else:
-            _, exc_instance, tb = sys.exc_info()
+            _, sys_exc_instance, tb = sys.exc_info()
+            if (
+                isinstance(sys_exc_instance, BaseException)
+                and not isinstance(sys_exc_instance, Exception)
+            ):
+                # bypass BaseException as they are used for controlling the process/coroutine lifecycles
+                # instead of indicating actual errors
+                return
         exc_type: Any = type(exc_instance)
 
         if context is None or 'severity' not in context:
