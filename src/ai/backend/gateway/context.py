d4474a8d1b1bb737b8905058b2331286798f13df
Joongi Kim <joongi@lablup.com>
2021-02-22 22:05:20 +0900

    WIP: add missing file

---
+++ src/ai/backend/gateway/context.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Sequence, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from aiopg.sa.engine import _PoolAcquireContextManager as SAPool
+    from aioredis import Redis
+
+    from ai.backend.common.events import EventDispatcher, EventProducer
+    from ai.backend.common.plugin.hook import HookPluginContext
+    from ai.backend.common.plugin.monitor import ErrorPluginContext, StatsPluginContext
+
+    from ..manager.background import BackgroundTaskManager
+    from ..manager.models.storage import StorageSessionManager
+    from ..manager.idle import BaseIdleChecker
+    from ..manager.plugin.webapp import WebappPluginContext
+    from ..manager.registry import AgentRegistry
+    from .config import LocalConfig, SharedConfig
+    from .types import CORSOptions
+
+
+class BaseContext:
+    pass
+
+
+class RootContext(BaseContext):
+    pidx: int
+    dbpool: SAPool
+    event_dispatcher: EventDispatcher
+    event_producer: EventProducer
+    redis_live: Redis
+    redis_stat: Redis
+    redis_image: Redis
+    redis_stream: Redis
+    shared_config: SharedConfig
+    local_config: LocalConfig
+    cors_options: CORSOptions
+
+    webapp_plugin_ctx: WebappPluginContext
+    idle_checkers: Sequence[BaseIdleChecker]
+    storage_manager: StorageSessionManager
+    hook_plugin_ctx: HookPluginContext
+
+    registry: AgentRegistry
+
+    error_monitor: ErrorPluginContext
+    stats_monitor: StatsPluginContext
+    background_task_manager: BackgroundTaskManager
