69a9209dd0f6e6bc1e415dfa40639eb5722a3b83
Sanghun Lee <sanghun@lablup.com>
2022-03-25 15:46:37 +0900

    fix: use SQL func

--- src/ai/backend/manager/models/alembic/versions/11146ba02235_change_char_col_to_str.py
+++ src/ai/backend/manager/models/alembic/versions/11146ba02235_change_char_col_to_str.py
@@ -18,22 +18,12 @@ depends_on = None
 
 def upgrade():
     conn = op.get_bind()
-    agents = sa.table('agents',
-        sa.Column('id', sa.String), sa.Column('architecture', sa.String),
-    )
-    result = conn.execute(sa.select([agents.c.id, agents.c.architecture])).all()
-    updates = []
-    for row in result:
-        if row.architecture != row.architecture.rstrip():
-            print(f'len(row.architecture) = {len(row.architecture)}')
-            updates.append({'row_id': row.id, 'architecture': row.architecture.rstrip()})
     op.alter_column('agents', column_name='architecture', type_=sa.String(length=32))
-    query = (
-        sa.update(agents)
-        .values(architecture = bindparam('architecture'))
-        .where(agents.c.id == bindparam('row_id'))
-    )
-    conn.execute(query, updates)
+    query = '''
+    UPDATE agents
+    SET architecture = TRIM (architecture);
+    '''
+    conn.execute(query)
 
 def downgrade():
     op.alter_column('agents', column_name='architecture', type_=sa.CHAR(length=32))
