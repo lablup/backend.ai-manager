lablup/backend.ai-manager

commit c62ae6575f9ae6e44541fdab2df98d7116941479

Add an option to auto-cancel a pending session after specified timeout (#527)
* Update `_list_pending_sessions()` to separate out to-be-cancelled
  sessions by checking pending timeout.
  - Mark those sessions cancelled with 'pending-timeout' status_info
    before invoking the scheduler.
  - When they are cancelled, produce `SessionCancelledEvent` for
    pending-timeout sessions.

* Let `models.base.populate_fixture()` handle `StructuredJSONObjectColumn`
  and `StructuredJSONObjectListColumn` correctly.

* refactor: Change `scaling_groups.scheduler_opts` to use
  `StructuredJSONObjectColumn` a typed attr class.
  - Scheduler configs are now part of it as the "config" field instead of
    scheduler-specific name fields such as "fifo".
  - NOTE: The etcd's scheduler plugin config format is NOT changed.
  - Fix merging of global scheduler config and local scheduler config.

* test: Refactor out `registry_ctx` as a global pytest fixture.

Co-authored-by: Joongi Kim <joongi@lablup.com>

src/ai/backend/manager/scheduler/dispatcher.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
 import asyncio
-from contextvars import ContextVar
-from datetime import datetime
 import logging
 import pkg_resources
+from contextvars import ContextVar
+from datetime import datetime, timedelta
 from typing import (
     Any,
     Awaitable,
     Final,
     List,
-    Mapping,
     Sequence,
     Tuple,
     Union,
@@ -21,6 +20,7 @@ from typing import (
 import aiotools
 from dateutil.tz import tzutc
 import sqlalchemy as sa
+from sqlalchemy.engine.row import Row
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.ext.asyncio import (
     AsyncConnection as SAConnection,
@@ -61,6 +61,7 @@ from ..models import (
     AgentStatus, KernelStatus,
     AGENT_RESOURCE_OCCUPYING_KERNEL_STATUSES,
 )
+from ..models.scaling_group import ScalingGroupOpts
 from ..models.utils import (
     ExtendedAsyncSAEngine as SAEngine,
     execute_with_retry,
@@ -104,14 +105,17 @@ _log_args: ContextVar[Tuple[Any, ...]] = ContextVar('_log_args')
 _key_schedule_prep_tasks: Final = "scheduler.preptasks"
 
 
-def load_scheduler(name: str, scheduler_configs: Mapping[str, Any]) -> AbstractScheduler:
+def load_scheduler(
+    name: str,
+    sgroup_opts: ScalingGroupOpts,
+    scheduler_config: dict[str, Any],
+) -> AbstractScheduler:
     entry_prefix = 'backendai_scheduler_v10'
     for entrypoint in pkg_resources.iter_entry_points(entry_prefix):
         if entrypoint.name == name:
             log.debug('loading scheduler plugin "{}" from {}', name, entrypoint.module_name)
             scheduler_cls = entrypoint.load()
-            scheduler_config = scheduler_configs.get(name, {})
-            return scheduler_cls(scheduler_config)
+            return scheduler_cls(sgroup_opts, scheduler_config)
     raise ImportError('Cannot load the scheduler plugin', name)
 
 
@@ -255,12 +259,12 @@ class SchedulerDispatcher(aobject):
         result = await db_conn.execute(query)
         row = result.first()
         scheduler_name = row['scheduler']
-        local_scheduler_opts = row['scheduler_opts']
+        sgroup_opts: ScalingGroupOpts = row['scheduler_opts']
         global_scheduler_opts = {}
         if self.shared_config['plugins']['scheduler']:
-            global_scheduler_opts = self.shared_config['plugins']['scheduler'][scheduler_name]
-        scheduler_opts = {**global_scheduler_opts, **local_scheduler_opts}
-        return load_scheduler(scheduler_name, scheduler_opts)
+            global_scheduler_opts = self.shared_config['plugins']['scheduler'].get(scheduler_name, {})
+        scheduler_specific_config = {**global_scheduler_opts, **sgroup_opts.config}
+        return load_scheduler(scheduler_name, sgroup_opts, scheduler_specific_config)
 
     async def _schedule_in_sgroup(
         self,
@@ -269,15 +273,45 @@ class SchedulerDispatcher(aobject):
     ) -> None:
         async with self.db.begin_readonly() as kernel_db_conn:
             scheduler = await self._load_scheduler(kernel_db_conn, sgroup_name)
-            pending_sessions = await _list_pending_sessions(kernel_db_conn, sgroup_name)
+            pending_session_rows, cancelled_session_rows = \
+                await _list_pending_sessions(kernel_db_conn, scheduler, sgroup_name)
+            pending_sessions = PendingSession.from_rows(pending_session_rows)
             existing_sessions = await _list_existing_sessions(kernel_db_conn, sgroup_name)
-        log.debug('running scheduler (sgroup:{}, pending:{}, existing:{})',
-                  sgroup_name, len(pending_sessions), len(existing_sessions))
+
+        if cancelled_session_rows:
+            now = datetime.now(tzutc())
+
+            async def _apply_cancellation():
+                async with self.db.begin() as db_conn:
+                    query = kernels.update().values({
+                        'status': KernelStatus.CANCELLED,
+                        'status_changed': now,
+                        'status_info': "pending-timeout",
+                        'terminated_at': now,
+                    }).where(kernels.c.session_id.in_([
+                        item['session_id'] for item in cancelled_session_rows
+                    ]))
+                    await db_conn.execute(query)
+
+            await execute_with_retry(_apply_cancellation)
+            for item in cancelled_session_rows:
+                await self.event_producer.produce_event(
+                    SessionCancelledEvent(
+                        item['session_id'],
+                        item['session_creation_id'],
+                        reason="pending timeout",
+                    ),
+                )
+
+        log.debug(
+            "running scheduler (sgroup:{}, pending:{}, existing:{}, cancelled:{})",
+            sgroup_name, len(pending_sessions), len(existing_sessions), len(cancelled_session_rows),
+        )
         zero = ResourceSlot()
         num_scheduled = 0
         while len(pending_sessions) > 0:
 
-            async with self.db.begin() as conn:
+            async with self.db.begin_readonly() as conn:
                 candidate_agents = await _list_agents_by_sgroup(conn, sgroup_name)
                 total_capacity = sum((ag.available_slots for ag in candidate_agents), zero)
 
@@ -396,7 +430,7 @@ class SchedulerDispatcher(aobject):
                         query = kernels.update().values({
                             'status_info': "predicate-checks-failed",
                             'status_data': sql_json_increment(
-                                kernels.c.status_data,
+                                kernels.c.stats_data,
                                 ('scheduler', 'retries'),
                                 parent_updates={
                                     'last_try': datetime.now(tzutc()).isoformat(),
@@ -850,8 +884,10 @@ class SchedulerDispatcher(aobject):
 
 async def _list_pending_sessions(
     db_conn: SAConnection,
+    scheduler: AbstractScheduler,
     sgroup_name: str,
-) -> List[PendingSession]:
+) -> tuple[list[Row], list[Row]]:
+    pending_timeout: timedelta = scheduler.sgroup_opts.pending_timeout
     query = (
         PendingSession.base_query()
         .where(
@@ -862,7 +898,16 @@ async def _list_pending_sessions(
         )
     )
     rows = (await db_conn.execute(query)).fetchall()
-    return PendingSession.from_rows(rows)
+    candidate_rows = []
+    cancelled_rows = []
+    now = datetime.now(tzutc())
+    for row in rows:
+        elapsed_pending_time = now - row['created_at']
+        if pending_timeout.total_seconds() > 0 and elapsed_pending_time >= pending_timeout:
+            cancelled_rows.append(row)
+        else:
+            candidate_rows.append(row)
+    return candidate_rows, cancelled_rows
 
 
 async def _list_existing_sessions(

src/ai/backend/manager/models/base.py
@@ -219,7 +219,14 @@ class StructuredJSONColumn(TypeDecorator):
     def process_bind_param(self, value, dialect):
         if value is None:
             return self._schema.check({})
-        return self._schema.check(value)
+        try:
+            self._schema.check(value)
+        except t.DataError as e:
+            raise ValueError(
+                "The given value does not conform with the structured json column format.",
+                e.as_dict(),
+            )
+        return value
 
     def process_result_value(self, raw_value, dialect):
         if raw_value is None:
@@ -238,18 +245,18 @@ class StructuredJSONObjectColumn(TypeDecorator):
     impl = JSONB
     cache_ok = True
 
-    def __init__(self, attr_cls: Type[JSONSerializableMixin]) -> None:
+    def __init__(self, schema: Type[JSONSerializableMixin]) -> None:
         super().__init__()
-        self._attr_cls = attr_cls
+        self._schema = schema
 
     def process_bind_param(self, value, dialect):
-        return self._attr_cls.to_json(value)
+        return self._schema.to_json(value)
 
     def process_result_value(self, raw_value, dialect):
-        return self._attr_cls.from_json(raw_value)
+        return self._schema.from_json(raw_value)
 
     def copy(self):
-        return StructuredJSONObjectColumn(self._attr_cls)
+        return StructuredJSONObjectColumn(self._schema)
 
 
 class StructuredJSONObjectListColumn(TypeDecorator):
@@ -261,20 +268,20 @@ class StructuredJSONObjectListColumn(TypeDecorator):
     impl = JSONB
     cache_ok = True
 
-    def __init__(self, attr_cls: Type[JSONSerializableMixin]) -> None:
+    def __init__(self, schema: Type[JSONSerializableMixin]) -> None:
         super().__init__()
-        self._attr_cls = attr_cls
+        self._schema = schema
 
     def process_bind_param(self, value, dialect):
-        return [self._attr_cls.to_json(item) for item in value]
+        return [self._schema.to_json(item) for item in value]
 
     def process_result_value(self, raw_value, dialect):
         if raw_value is None:
             return []
-        return [self._attr_cls.from_json(item) for item in raw_value]
+        return [self._schema.from_json(item) for item in raw_value]
 
     def copy(self):
-        return StructuredJSONObjectListColumn(self._attr_cls)
+        return StructuredJSONObjectListColumn(self._schema)
 
 
 class CurrencyTypes(enum.Enum):
@@ -797,4 +804,7 @@ async def populate_fixture(
                 elif isinstance(col.type, EnumValueType):
                     for row in rows:
                         row[col.name] = col.type._enum_cls(row[col.name])
+                elif isinstance(col.type, (StructuredJSONObjectColumn, StructuredJSONObjectListColumn)):
+                    for row in rows:
+                        row[col.name] = col.type._schema.from_json(row[col.name])
             await conn.execute(sa.dialects.postgresql.insert(table, rows).on_conflict_do_nothing())

src/ai/backend/manager/models/scaling_group.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from datetime import timedelta
 from typing import (
     Any,
     Dict,
+    Mapping,
     Sequence,
     Set,
     TYPE_CHECKING,
@@ -10,6 +12,7 @@ from typing import (
 )
 import uuid
 
+import attr
 import graphene
 from graphene.types.datetime import DateTime as GQLDateTime
 import sqlalchemy as sa
@@ -19,7 +22,7 @@ from sqlalchemy.ext.asyncio import AsyncConnection as SAConnection
 import trafaret as t
 
 from ai.backend.common import validators as tx
-from ai.backend.common.types import SessionTypes
+from ai.backend.common.types import SessionTypes, JSONSerializableMixin
 
 from .base import (
     metadata,
@@ -28,7 +31,7 @@ from .base import (
     set_if_set,
     batch_result,
     batch_multiresult,
-    StructuredJSONColumn,
+    StructuredJSONObjectColumn,
 )
 from .group import resolve_group_name_or_id
 from .user import UserRole
@@ -57,6 +60,39 @@ __all__: Sequence[str] = (
 )
 
 
+@attr.define(slots=True)
+class ScalingGroupOpts(JSONSerializableMixin):
+    allowed_session_types: list[SessionTypes] = attr.Factory(
+        lambda: [SessionTypes.INTERACTIVE, SessionTypes.BATCH],
+    )
+    pending_timeout: timedelta = timedelta(seconds=0)
+    config: Mapping[str, Any] = attr.Factory(dict)
+
+    def to_json(self) -> dict[str, Any]:
+        return {
+            "allowed_session_types": [
+                item.value for item in self.allowed_session_types
+            ],
+            "pending_timeout": self.pending_timeout.total_seconds(),
+            "config": self.config,
+        }
+
+    @classmethod
+    def from_json(cls, obj: Mapping[str, Any]) -> ScalingGroupOpts:
+        return cls(**cls.as_trafaret().check(obj))
+
+    @classmethod
+    def as_trafaret(cls) -> t.Trafaret:
+        return t.Dict({
+            t.Key('allowed_session_types', default=['interactive', 'batch']):
+                t.List(tx.Enum(SessionTypes), min_length=1),
+            t.Key('pending_timeout', default=0):
+                tx.TimeDuration(allow_negative=False),
+            # Each scheduler impl refers an additional "config" key.
+            t.Key("config", default={}): t.Mapping(t.String, t.Any),
+        }).allow_extra('*')
+
+
 scaling_groups = sa.Table(
     'scaling_groups', metadata,
     sa.Column('name', sa.String(length=64), primary_key=True),
@@ -68,12 +104,10 @@ scaling_groups = sa.Table(
     sa.Column('driver', sa.String(length=64), nullable=False),
     sa.Column('driver_opts', pgsql.JSONB(), nullable=False, default={}),
     sa.Column('scheduler', sa.String(length=64), nullable=False),
-    sa.Column('scheduler_opts', StructuredJSONColumn(
-        t.Dict({
-            t.Key('allowed_session_types', default=['interactive', 'batch']):
-                t.List(tx.Enum(SessionTypes), min_length=1),
-        }).allow_extra('*'),
-    ), nullable=False, default={}),
+    sa.Column(
+        'scheduler_opts', StructuredJSONObjectColumn(ScalingGroupOpts),
+        nullable=False, default={},
+    ),
 )
 
 
@@ -202,7 +236,7 @@ class ScalingGroup(graphene.ObjectType):
             driver=row['driver'],
             driver_opts=row['driver_opts'],
             scheduler=row['scheduler'],
-            scheduler_opts=row['scheduler_opts'],
+            scheduler_opts=row['scheduler_opts'].to_json(),
         )
 
     @classmethod

src/ai/backend/manager/scheduler/drf.py
@@ -19,6 +19,8 @@ from ai.backend.common.types import (
     ResourceSlot,
     SessionId,
 )
+
+from ..models.scaling_group import ScalingGroupOpts
 from .types import (
     AbstractScheduler,
     AgentContext,
@@ -36,8 +38,8 @@ class DRFScheduler(AbstractScheduler):
     per_user_dominant_share: Dict[AccessKey, Decimal]
     total_capacity: ResourceSlot
 
-    def __init__(self, config: Mapping[str, Any]) -> None:
-        super().__init__(config)
+    def __init__(self, sgroup_opts: ScalingGroupOpts, config: Mapping[str, Any]) -> None:
+        super().__init__(sgroup_opts, config)
         self.per_user_dominant_share = defaultdict(lambda: Decimal(0))
 
     def pick_session(

src/ai/backend/manager/scheduler/fifo.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
-
 from decimal import Decimal
 from typing import (
-    Any,
     List,
     Optional,
     Sequence,
-    Mapping,
     Tuple,
 )
 
@@ -119,9 +116,6 @@ class FIFOSlotScheduler(AbstractScheduler):
 class LIFOSlotScheduler(AbstractScheduler):
 
     config_iv = t.Dict({}).allow_extra('*')
-
-    def __init__(self, config: Mapping[str, Any]) -> None:
-        super().__init__(config)
 
     def pick_session(
         self,

src/ai/backend/manager/scheduler/mof.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
 from typing import (
-    Any,
     Optional,
     Sequence,
-    Mapping,
 )
 
 import trafaret as t
@@ -29,9 +27,6 @@ class MOFScheduler(AbstractScheduler):
     """Minimum Occupied slot First Scheduler"""
 
     config_iv = t.Dict({}).allow_extra('*')
-
-    def __init__(self, config: Mapping[str, Any]) -> None:
-        super().__init__(config)
 
     def pick_session(
         self,

src/ai/backend/manager/scheduler/predicates.py
@@ -270,7 +270,7 @@ async def check_scaling_group(
                 f"You do not have access to the scaling group '{preferred_sgroup_name}'.",
                 permanent=True,
             )
-        allowed_session_types = sgroup['scheduler_opts']['allowed_session_types']
+        allowed_session_types = sgroup['scheduler_opts'].allowed_session_types
         if sess_ctx.session_type.value.lower() not in allowed_session_types:
             return PredicateResult(
                 False,
@@ -283,7 +283,7 @@ async def check_scaling_group(
         # Consider all allowed scaling groups.
         usable_sgroups = []
         for sgroup in sgroups:
-            allowed_session_types = sgroup['scheduler_opts']['allowed_session_types']
+            allowed_session_types = sgroup['scheduler_opts'].allowed_session_types
             if sess_ctx.session_type.value.lower() in allowed_session_types:
                 usable_sgroups.append(sgroup)
         if not usable_sgroups:

src/ai/backend/manager/scheduler/types.py
@@ -21,6 +21,7 @@ import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncConnection as SAConnection
 from sqlalchemy.sql import Select, ColumnElement
 from sqlalchemy.engine.row import Row
+from datetime import datetime
 import trafaret as t
 
 from ai.backend.common.logging import BraceStyleAdapter
@@ -44,6 +45,7 @@ from ..defs import DEFAULT_ROLE
 from ..models import (
     kernels, keypairs,
 )
+from ..models.scaling_group import ScalingGroupOpts
 from ..registry import AgentRegistry
 
 log = BraceStyleAdapter(logging.getLogger('ai.backend.manager.scheduler'))
@@ -201,6 +203,7 @@ class PendingSession:
     startup_command: Optional[str]
     internal_data: Optional[MutableMapping[str, Any]]
     preopen_ports: List[int]
+    created_at: datetime
 
     @property
     def main_kernel_id(self) -> KernelId:
@@ -235,6 +238,7 @@ class PendingSession:
             kernels.c.bootstrap_script,
             kernels.c.startup_command,
             kernels.c.preopen_ports,
+            kernels.c.created_at,
         }
 
     @classmethod
@@ -280,6 +284,7 @@ class PendingSession:
             bootstrap_script=row['bootstrap_script'],
             startup_command=row['startup_command'],
             preopen_ports=row['preopen_ports'],
+            created_at=row['created_at'],
         )
 
     @classmethod
@@ -316,6 +321,7 @@ class KernelInfo:
     requested_slots: ResourceSlot
     bootstrap_script: Optional[str]
     startup_command: Optional[str]
+    created_at: datetime
 
     def __str__(self):
         return f'{self.kernel_id}#{self.cluster_role}{self.cluster_idx}'
@@ -338,6 +344,7 @@ class KernelInfo:
             kernels.c.occupied_slots,
             kernels.c.bootstrap_script,
             kernels.c.startup_command,
+            kernels.c.created_at,
         }
 
     @classmethod
@@ -356,6 +363,7 @@ class KernelInfo:
             requested_slots=row['occupied_slots'],
             bootstrap_script=row['bootstrap_script'],
             startup_command=row['startup_command'],
+            created_at=row['created_at'],
         )
 
 
@@ -389,10 +397,12 @@ class AbstractScheduler(metaclass=ABCMeta):
     ``schedule()`` method is a pure function.
     """
 
-    config: Mapping[str, Any]
+    sgroup_opts: ScalingGroupOpts  # sgroup-specific config
+    config: Mapping[str, Any]   # scheduler-specific config
     config_iv: t.Dict
 
-    def __init__(self, config: Mapping[str, Any]) -> None:
+    def __init__(self, sgroup_opts: ScalingGroupOpts, config: Mapping[str, Any]) -> None:
+        self.sgroup_opts = sgroup_opts
         self.config = self.config_iv.check(config)
 
     @abstractmethod

commit 5e90c714d1d408d901e7d0264ca6a5204eeeec86

hotfix: A regression in #527

src/ai/backend/manager/scheduler/dispatcher.py
@@ -432,7 +432,7 @@ class SchedulerDispatcher(aobject):
                         query = kernels.update().values({
                             'status_info': "predicate-checks-failed",
                             'status_data': sql_json_increment(
-                                kernels.c.stats_data,
+                                kernels.c.status_data,
                                 ('scheduler', 'retries'),
                                 parent_updates={
                                     'last_try': datetime.now(tzutc()).isoformat(),