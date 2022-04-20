c3baea8ece1548b332e1c05e8d8a083f0cb59507
Joongi Kim <joongi@lablup.com>
2022-04-20 03:21:20 +0000

    refactor: Separate use cases of URLNotFound and ObjectNotFound

--- src/ai/backend/manager/server.py
+++ src/ai/backend/manager/server.py
@@ -49,7 +49,7 @@ from .api.context import RootContext
 from .api.exceptions import (
     BackendError,
     MethodNotAllowed,
-    GenericNotFound,
+    URLNotFound,
     GenericBadRequest,
     InternalServerError,
     InvalidAPIParameters,
@@ -225,7 +225,7 @@ async def exception_middleware(request: web.Request,
         await stats_monitor.report_metric(INCREMENT, 'ai.backend.manager.api.failures')
         await stats_monitor.report_metric(INCREMENT, f'ai.backend.manager.api.status.{ex.status_code}')
         if ex.status_code == 404:
-            raise GenericNotFound(f"Unknown URL path: {request.path}")
+            raise URLNotFound(extra_data=request.path)
         if ex.status_code == 405:
             concrete_ex = cast(web.HTTPMethodNotAllowed, ex)
             raise MethodNotAllowed(concrete_ex.method, concrete_ex.allowed_methods)
