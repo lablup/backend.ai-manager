0a91b8cc499c0a814a1169544d3ba2136e44431d
DaeHyun Sung <dhsung@lablup.com>
2022-05-10 17:19:36 +0900

    Apply to wrap UnknownImageReference as InvalidAPIParameters
    Apply to wrap UnknownImageReference
    1. Define `UnknownImageReferenceError` exception
    2. Apply to wrap UnknownImageReference as InvalidAPIParameters instead of InternalServerError

--- src/ai/backend/manager/api/exceptions.py
+++ src/ai/backend/manager/api/exceptions.py
@@ -412,3 +412,8 @@ class KernelRestartFailed(BackendAgentError, web.HTTPInternalServerError):
 class KernelExecutionFailed(BackendAgentError, web.HTTPInternalServerError):
     error_type  = 'https://api.backend.ai/probs/kernel-execution-failed'
     error_title = 'Executing user code in the kernel has failed.'
+
+
+class UnknownImageReferenceError(BackendError, web.HTTPBadRequest):
+    error_type = 'https://api.backend.ai/probs/unknown-image-reference-error'
+    error_title = 'Unknown Image Reference'
